04b154c8621cc31f8d6f5a1ebe851f51f923f68f
KalpeshJetani <kalpeshjetani@openxcell.info>
2017-03-24 19:08:06 +0530

    Done

--- SegueTest/ViewController.swift
+++ SegueTest/ViewController.swift
@@ -19,7 +19,45 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func unwindSegue_ViewController(segue : UIStoryboardSegue){
+        
+    }
 
 
 }
 
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindSegue_FirstViewController(segue : UIStoryboardSegue){
+        
+    }
+}
+
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindSegue_SecondViewController(segue : UIStoryboardSegue){
+        
+    }
+}
